0ca35e45fc1460880bbc1d34d5c165449400d3b5
Aman Kalal <amankalal206@gmail.com>
2025-05-10 03:35:44 +0530

    added meta tags

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,10 +8,23 @@ import Philosophy from "@/components/landingPage/Philosophy";
 import { Banner } from "@workspace/ui/components/banner";
 import { SkiperCardDemo } from "@/components/ui/HowTo";
 import { BannerNewFeature } from "@/components/landingPage/Banner";
+import Head from "next/head";
 
 export default function Page() {
   return (
     <div className=" flex flex-col overflow-hidden bg-gradient-to-tr from-yellow-500/10 to-transparent ">
+      <Head>
+        <title>Amazing Website Title</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="kreateAI" />
+        <meta
+          name="twitter:description"
+          content="Create high-quality tweets by just pasting video links in seconds"
+        />
+        <meta name="twitter:image" content="/kreateAI.png" />
+        <meta name="twitter:url" content="https://thread-ai-web.vercel.app/" />
+        <meta name="twitter:site" content="@0xAmansol" />
+      </Head>
       <Navbar />
       <main>
         <Hero2 />
